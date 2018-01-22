239a0a51ffa9624413ccd2f47b0efed50bab3bcf
Unknown <whatcharawali17@gmail.com>
2018-01-22 17:59:48 +0700

    เสร็จแล้วข้อที่ 5

--- lab_7_5.cpp
+++ lab_7_5.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main () {
 
-  int num = 36;
+  int num;
   // การหาเลขคู่ และแสดงเฉพาะเลขคู่
-  if ((num % 2) == 0)) {
+  for (num = 0; num <= 65535; num++)
+  {
+
+  if ((num % 2) == 0)
+  {
     cout << num << endl;
+  }
   }
 
   return 0;
