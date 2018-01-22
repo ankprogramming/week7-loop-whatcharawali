8d9e8893c105a8b63433844c2142b979567b4b52
Unknown <whatcharawali17@gmail.com>
2018-01-22 17:52:17 +0700

    เสร็จแล้วข้อที่ 4

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -40,7 +40,21 @@ int main () {
   cout << "*" << endl;
 
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
+ for (int i = 0; i < 3; i++)
+ {
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "***" << endl;
+  cout << "****" << endl;
+  cout << "*****" << endl;
+  cout << "******" << endl;
+  cout << "*****" << endl;
+  cout << "****" << endl;
+  cout << "***" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
 
+ }
 
   return 0;
 }
