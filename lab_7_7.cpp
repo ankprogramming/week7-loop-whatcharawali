202d5c47a10c63707f9d34f1a50020e831a402c9
Unknown <whatcharawali17@gmail.com>
2018-01-22 18:21:48 +0700

    เสร็จแล้วข้อที่ 7

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -5,6 +5,17 @@ using namespace std;
 int main () {
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
+  int n;
+  int sum = 0;
+  float avg;
 
+  for (int i=0; i < 5; i++) {
+   cin >> n;
+   sum += n;
+  }
+
+ // cout << sum;
+  avg = (float)sum / 5;
+  cout << avg << endl;
   return 0;
 }
